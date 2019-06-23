30be41acd31865d063469b23ffb5b6e1797520ec
Luminighty <balazs922@gmail.hu>
2019-06-23 15:37:22 +0200

    Notifications

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import React, {Component} from 'react';
-import {View, TextInput, Button, Alert, AppState, StyleSheet, Text, PushNotificationIOS} from 'react-native';
+import React, {
+	Component
+} from 'react';
+import {
+	View,
+	TextInput,
+	Button,
+	Alert,
+	AppState,
+	StyleSheet,
+	Text,
+	PushNotificationIOS
+} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
 const styles = StyleSheet.create({
@@ -12,31 +23,44 @@ const styles = StyleSheet.create({
 })
 
 export default class App extends Component {
-	state = {appState: AppState.currentState};
-	
 	componentDidMount() {
+		AppState.addEventListener('change', this._handleAppStateChange);
 		PushNotification.configure({
-			onNotification: function(notification) {
-				console.log( 'NOTIFICATION:', notification );
+			onNotification: function (notification) {
+				console.log('NOTIFICATION:', notification);
 
 				// process the notification
 				let alertData = notification.data;
-				if(!notification.foreground)
+
+				console.log("APPSTATE 1:", AppState.currentState);
+				if (AppState.currentState !== "active")
 					alertData *= 2;
-				alert(alertData);
-				console.log("APPSTATE:", AppState.currentState);
+				alert(alertData + "asd");
 				// required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
 				notification.finish(PushNotificationIOS.FetchResult.NoData);
 			},
-			requestPermissions: true
+			requestPermissions: false,
+			popInitialNotification: true
 		});
 	}
+
+	componentWillUnmount() {
+		AppState.removeEventListener('change', this._handleAppStateChange);
+	}
+
+	_handleAppStateChange = (nextAppState) => {
+		this.setState({
+			appState: nextAppState
+		});
+	};
+
 	constructor(props) {
 		super(props);
 		this.state = {
 			"title": "",
 			"body": "",
-			"data": 3
+			"data": 3,
+			appState: AppState.currentState
 		};
 		this.onTextChanged = this.onTextChanged.bind(this);
 		this.onButtonPress = this.onButtonPress.bind(this);
@@ -49,6 +73,8 @@ export default class App extends Component {
 	}
 
 	onButtonPress() {
+		console.log("APPSTATE", AppState.currentState);
+
 		PushNotification.localNotificationSchedule({
 			title: this.state.title,
 			message: this.state.body,
@@ -58,48 +84,52 @@ export default class App extends Component {
 	}
 
 	render() {
-		return (
-			<View style={{marginHorizontal: 20, marginTop: 50}}>
+		return ( 
+			<View style = {{marginHorizontal: 20,marginTop: 50}} >
 				<Text>Title</Text>
-				<TextInput style={styles.input} onChangeText={(text) => this.onTextChanged(text, "title")}/>
+				<TextInput style = {styles.input}onChangeText = {(text) => this.onTextChanged(text, "title")}/>
 				<Text>Body</Text>
-				<TextInput style={styles.input} onChangeText={(text) => this.onTextChanged(text, "body")}/>
-				<Text>Data</Text>
-				<NumberInput min="3" max="10" style={styles.input} onValueChange={(val) => this.onTextChanged(val, "data")}/>
-				<Button title="Button" onPress={() => (this.onButtonPress(this.state))}/>
+				<TextInput style = {styles.input} onChangeText = {(text) => this.onTextChanged(text, "body")}/> 
+				<Text>Data</Text> 
+				<NumberInput min = "3" max = "10" style = {styles.input} onValueChange = {(val) => this.onTextChanged(val, "data")}/> 
+				<Button title = "Button" onPress = {this.onButtonPress}/> 
 			</View>
 		);
-	}	
+	}
 }
 
 class NumberInput extends TextInput {
 
 	constructor(props) {
 		super(props);
-		this.state = {value:""}
+		this.state = {
+			value: ""
+		}
 		this.validateNumber = this.validateNumber.bind(this);
 	}
 
 	validateNumber(e) {
-		if(e !== '' && e !== "-") {
-			if(isNaN(e.toString()))
+		if (e !== '' && e !== "-") {
+			if (isNaN(e.toString()))
 				return;
 			const num = Number(e);
 			const min = this.props.min;
 			const max = this.props.max;
-			if(min !== undefined && Number(min) > num)
+			if (min !== undefined && Number(min) > num)
 				e = min;
-			if(max !== undefined && Number(max) < num)
+			if (max !== undefined && Number(max) < num)
 				e = max;
 		}
-		this.setState({value: e});
-		if(this.props.onValueChange !== undefined)
+		this.setState({
+			value: e
+		});
+		if (this.props.onValueChange !== undefined)
 			this.props.onValueChange(e);
 	}
 
 	render() {
-		return (
-			<TextInput {...this.props} keyboardType="numeric" onChangeText={this.validateNumber} value={this.state.value}/>
+		return ( 
+		<TextInput {...this.props} keyboardType = "numeric" onChangeText = {this.validateNumber} value = {this.state.value}/>
 		);
 	}
 
